e8aa3718612b144f300ff0b378f1626b9d957e09
Jianjun Chen (Edge) <jianjunchen@microsoft.com>
2025-04-12 15:48:30 +0800

    exclude vendor

--- src/conf/conf.go
+++ src/conf/conf.go
@@ -31,7 +31,7 @@ var (
 	DefaultInclude = []string{"*.cc", "*.c", "*.hpp", "*.cpp", "*.h", "*.md", "*.js", "*.ts", "*.txt", "*.mm", "*.java",
 		"*.cs", "*.py", "*.kt", "*.go", "*.rb", "*.php", "*.html", "*.css", "*.yaml", "*.yml", "*.toml", "*.xml", "*.sql",
 		"*.sh", "Makefile", "*.bat", "*.ps1", "*.sln", "*.json", "*.vcxproj", "*.vcproj", "*.vcxproj.filters"}
-	DefaultExclude = []string{"node_modules/", "dist/", "build/", "out/", "obj/", "log/", "logs/", ".*", "*.log",
+	DefaultExclude = []string{"node_modules/", "dist/", "build/", "vendor/", "out/", "obj/", "log/", "logs/", ".*", "*.log",
 		"*.log.*", "*log.txt"}
 )
 
